4ac581567fe3fc38649d228c27944aba9dacfbdb
ritikar97 <rira3427@colorado.edu>
2023-02-26 00:52:47 -0700

    sockettest.sh passes

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -48,8 +48,19 @@ int socketfd, socketFile_fd;
 
 status_flags s_flags = {.is_file_open = false, .is_log_open = false, .is_socket_open = false, .signal_caught = false};
 
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
 // Close log, socket, socketFile, delete the file
-static void exit_program(int socketFile_fd, int socket_fd)
+static void exit_program()
 {
     if(s_flags.is_log_open)
     {
@@ -63,7 +74,7 @@ static void exit_program(int socketFile_fd, int socket_fd)
 
     if(s_flags.is_socket_open)
     {
-        close(socket_fd);
+        close(socketfd);
     }
 
     if(s_flags.signal_caught)
@@ -73,6 +84,7 @@ static void exit_program(int socketFile_fd, int socket_fd)
 
 }
 
+
 // Referenced from Linux System Programming Chapter 10
 static void signal_handler(int signo)
 {
@@ -80,8 +92,8 @@ static void signal_handler(int signo)
     {
         syslog(LOG_INFO, "Caught signal, exiting: %s\n", strsignal(signo));
         s_flags.signal_caught = true;
-        exit_program(0, socketfd);
-        exit(1);
+        exit_program();
+        exit(EXIT_SUCCESS);
     }
     else
     {
@@ -91,21 +103,7 @@ static void signal_handler(int signo)
 }
 
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-
-
-
-
-int main()
+static int socket_server()
 {
     int status, num_bytes_recv, bytes_in_buf = 0;
     int num_buf_segments = 1;
@@ -113,7 +111,6 @@ int main()
     int num_bytes_change; // bytes to be written to/ to be read from file
     int bytes_in_file = 0;
     int clientfd;
-    struct addrinfo hints, *res;
     struct sockaddr_storage client_addr;
     socklen_t addr_size;
     char ip_str[INET6_ADDRSTRLEN];
@@ -121,77 +118,13 @@ int main()
     char* rx_packet = NULL;
     char* newline_offset = NULL;
 
-
-    // Signal initialization
-    if((signal(SIGINT, signal_handler) == SIG_ERR) || (signal(SIGTERM, signal_handler) == SIG_ERR)) 
-    {
-        syslog(LOG_ERR, "ERROR: signal() %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-
-    // Open system log for logging capability
-    openlog(NULL, LOG_CONS | LOG_PID | LOG_PERROR, LOG_USER);
-    s_flags.is_log_open = true;
-
-    // Create file to write into
-    socketFile_fd = open(PATH_SOCKETDATA_FILE, O_CREAT | O_APPEND | O_RDWR, S_IRWXU | S_IRGRP | S_IROTH);
-
-    printf("File opened\n");
     
-    if(socketFile_fd == -1)
-    {
-        syslog(LOG_ERR, "ERROR: open() %s\n", strerror(errno));
-        exit_program(0, 0);
-        return -1;
-    }
-
-
-    // Initialize and obtain socket address
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    status = getaddrinfo(NULL, PORT, &hints, &res);
-    if(status != 0)
-    {
-        syslog(LOG_ERR, "ERROR: getaddrinfo() %s\n", gai_strerror(status));
-        //TO-DO: Add other shutdown steps
-        return -1;
-    }
-
-    // Open socket
-    socketfd = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
-
-    if(socketfd == -1)
-    {
-        syslog(LOG_ERR, "ERROR: socket() %s\n", strerror(errno));
-        exit_program(0, 0);
-        return -1;
-    }
-
-    s_flags.is_socket_open = true;
-
-    // Bind the socket to an address
-    status = bind(socketfd, res -> ai_addr, res -> ai_addrlen);
-
-    if(status == -1)
-    {
-        syslog(LOG_ERR, "ERROR: bind() %s\n", strerror(errno));
-        exit_program(0, socketfd);
-        return -1;
-    }
-
-    // Result is not used anymore
-    freeaddrinfo(res);
 
     // Listen for connections on the socket
     status = listen(socketfd, BACKLOG);
     if(status == -1)
     {
         syslog(LOG_ERR, "ERROR: listen() %s\n", strerror(errno));
-        exit_program(0, socketfd);
         return -1;
     }
 
@@ -206,7 +139,6 @@ int main()
         if(clientfd == -1)
         {
             syslog(LOG_ERR, "ERROR: accept() %s\n", strerror(errno));
-            exit_program(0, socketfd);
             return -1;
         }
         else // Print IP address of client
@@ -223,7 +155,6 @@ int main()
         if(rx_buffer == NULL || rx_packet == NULL)
         {
             syslog(LOG_ERR, "ERROR: malloc failed\n");
-            exit_program(0, socketfd);
             return -1;
         }
         memset(rx_packet, 0, RX_PACKET_LEN);
@@ -231,6 +162,7 @@ int main()
         // Operate on data stream as long as packets are received 
         while((num_bytes_recv = recv(clientfd, rx_packet, RX_PACKET_LEN, 0)) > 0)
         {
+
             printf("Just received %s\n", rx_packet);
             // If the rx_buffer does not have enough space, realloc it
             if((num_buf_segments*RX_PACKET_LEN) - bytes_in_buf < num_bytes_recv)
@@ -240,92 +172,40 @@ int main()
                 if(rx_buffer == NULL)
                 {
                     syslog(LOG_ERR, "ERROR: realloc failed\n");
-                    exit_program(0, socketfd);
                     return -1;
                 }
             }
-
 
             memcpy((void*) (rx_buffer + bytes_in_buf), (void*) rx_packet, num_bytes_recv);
             bytes_in_buf += num_bytes_recv;
 
 
-            while((newline_offset = strchr(rx_buffer, (int)'\n')) != NULL)
+            while((newline_offset = memchr(rx_buffer, (int)'\n', bytes_in_buf)) != NULL)
             {            
-                num_bytes_change = (char*)newline_offset - (char*)rx_buffer + 1;
-                printf("Size of packet = %d\n", num_bytes_change);
+                                num_bytes_change = (char*)newline_offset - (char*)rx_buffer + 1;
 
                 if(num_bytes_change < 0)
                 {
                     syslog(LOG_ERR, "ERROR: Incorrect calculation\n");
                 }
-
-                /*socketFile_fd = open(PATH_SOCKETDATA_FILE, O_WRONLY | O_APPEND, S_IRWXU | S_IRGRP | S_IROTH);
-
-                if(socketFile_fd == -1)
-                {
-                    syslog(LOG_ERR, "ERROR: open() %s\n", strerror(errno));
-                    exit_program(0, socketfd);
-                    return -1;
-                }
-
-                s_flags.is_file_open = true;*/
                 
-                printf("Just before writing\n");
-
-                //char* rx_buffer_copy = rx_buffer;
-
                 byte_delta_in_file = write(socketFile_fd, rx_buffer, num_bytes_change);
 
                 if(byte_delta_in_file == -1)
                 {
                     printf("Error in write()\n");
                     syslog(LOG_ERR, "ERROR: write() %s\n", strerror(errno));
-                    exit_program(socketFile_fd, socketfd);
                     return -1;
                 }
                 else if(byte_delta_in_file < num_bytes_change)
                 {
-                    printf("NOT ALLL BYTES HAVE BEEN WRITTEN\n");
-                    exit_program(socketFile_fd, socketfd);
+                    syslog(LOG_ERR, "All bytes have not been written\n");
                     return -1;
                 }
 
                 bytes_in_file += byte_delta_in_file;
 
-                /*while((num_bytes_change!=0) && 
-                (byte_delta_in_file = write(socketFile_fd, rx_buffer_copy, num_bytes_change) != 0))
-                {
-                    printf("Writing to file with num_bytes_change = %d, byte_delta_in_file = %d\n", num_bytes_change, byte_delta_in_file);
-                    if(byte_delta_in_file == -1)
-                    {
-                        if(errno == EINTR)
-                        {
-                            continue;
-                        }
-                        printf("Error in write()\n");
-                        syslog(LOG_ERR, "ERROR: write() %s\n", strerror(errno));
-                        exit_program(socketFile_fd, socketfd);
-                        return -1;
-                    }
-
-                    num_bytes_change -= byte_delta_in_file;
-                    bytes_in_file += byte_delta_in_file;
-                    rx_buffer_copy += byte_delta_in_file;
-
-                    printf("After change num_bytes_change = %d\n", num_bytes_change);
-                    printf("After change bytes_in_file = %d\n", bytes_in_file);
-                }*/
-                
-                printf("Number of bytes in the file at this point = %d\n", bytes_in_file);
-
-                //close(socketFile_fd);
-                //s_flags.is_file_open = false;
-
-                printf("Number of bytes I am shifting in the buffer = %d\n", (int)(bytes_in_buf - ((char*)newline_offset - (char*)rx_buffer + 1)));
-                
                 bytes_in_buf -= ((char*)newline_offset - (char*)rx_buffer + 1);
-                printf("Number of bytes in buffer = %d\n", bytes_in_buf);
 
                 memcpy((void*)rx_buffer, (void*)newline_offset, (RX_PACKET_LEN * num_buf_segments) - ((char*)newline_offset - (char*)rx_buffer + 1));
                 memset(rx_buffer + bytes_in_buf, 0, (RX_PACKET_LEN * num_buf_segments) - bytes_in_buf);
@@ -336,52 +216,24 @@ int main()
 
                 lseek(socketFile_fd, 0, SEEK_SET);
 
-                /*socketFile_fd = open(PATH_SOCKETDATA_FILE, O_RDONLY, S_IRWXU | S_IRGRP | S_IROTH);
-                if(socketFile_fd == -1)
-                {
-                    syslog(LOG_ERR, "ERROR: open() %s\n", strerror(errno));
-                    exit_program(0, socketfd);
-                    return -1;
-                }
-                s_flags.is_file_open = true;*/
-
                 while(num_bytes_change != 0)
                 {
                     byte_delta_in_file = read(socketFile_fd, &tx_buffer[0], RX_PACKET_LEN);
-                    printf("Reading from file\n");
                     if(byte_delta_in_file == -1)
                     {
                         syslog(LOG_ERR, "ERROR: read()\n");
-                        exit_program(socketFile_fd, socketfd);
                         return -1;
                     }
 
                     int num_bytes_to_send = byte_delta_in_file;
 
-                    printf("Number of bytes to send out = %d\n", num_bytes_to_send);
-                    //int num_bytes_sent_per_call = 0;
                     int total_bytes_sent = 0;
-                    // Logic referenced from Linux System Programming Chapter 2
-                    /*while(num_bytes_to_send != 0 && (num_bytes_sent_per_call = send(clientfd, &tx_buffer[total_bytes_sent], num_bytes_to_send, 0) != 0))
-                    {
-                        if(num_bytes_sent_per_call == -1)
-                        {
-                            syslog(LOG_ERR, "ERROR: send() %s\n", strerror(errno));
-                            exit_program(socketFile_fd, socketfd);
-                            return -1;
-                        }
-                        num_bytes_to_send -= num_bytes_sent_per_call;
-                        total_bytes_sent += num_bytes_sent_per_call;
-                    }*/
 
                     total_bytes_sent = send(clientfd, &tx_buffer[0], num_bytes_to_send, 0);
-
-
                     num_bytes_change -= total_bytes_sent;
-                }
+                } // while there are bytes to read from the file
 
-                //close(socketFile_fd);
-                //s_flags.is_file_open = false;
+                newline_offset = NULL;
 
             } // While newline exists in buffer
 
@@ -389,12 +241,10 @@ int main()
 
         } // while data is being received 
 
-        printf("AESD\n");
 
         if(num_bytes_recv == -1)
         {
             syslog(LOG_ERR, "ERROR: recv() %s\n", strerror(errno));
-            exit_program(0, socketfd);
             return -1;
         }
 
@@ -406,14 +256,92 @@ int main()
 
     }
 
-    printf("Out of the while loop\n");
     close(socketfd);
     s_flags.is_socket_open = false;
 
-    exit_program(0, 0);
-
-    printf("Bye\n");
-
     return 0;
+}
+
+
+int main()
+{
+    int ret_val;
+    struct addrinfo hints, *res;
+    int status;
+    int opt_val = 1;
+
+    // Signal initialization
+    if((signal(SIGINT, signal_handler) == SIG_ERR) || (signal(SIGTERM, signal_handler) == SIG_ERR)) 
+    {
+        syslog(LOG_ERR, "ERROR: signal() %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+
+    // Open system log for logging capability
+    openlog(NULL, LOG_CONS | LOG_PID | LOG_PERROR, LOG_USER);
+    s_flags.is_log_open = true;
+
+    // Create file to write into
+    socketFile_fd = open(PATH_SOCKETDATA_FILE, O_CREAT | O_APPEND | O_RDWR, S_IRWXU | S_IRGRP | S_IROTH);
+
+    syslog(LOG_INFO, "File opened\n");
+    
+    if(socketFile_fd == -1)
+    {
+        syslog(LOG_ERR, "ERROR: open() %s\n", strerror(errno));
+        exit_program();
+        return -1;
+    }
+
+    // Initialize and obtain socket address
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    status = getaddrinfo(NULL, PORT, &hints, &res);
+    if(status != 0)
+    {
+        syslog(LOG_ERR, "ERROR: getaddrinfo() %s\n", gai_strerror(status));
+        exit_program();
+        return -1;
+    }
+
+    // Open socket
+    socketfd = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
+
+    if(socketfd == -1)
+    {
+        syslog(LOG_ERR, "ERROR: socket() %s\n", strerror(errno));
+        return -1;
+    }
+
+    s_flags.is_socket_open = true;
+
+    if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEADDR, &opt_val, sizeof(int)) == -1) 
+    {
+        syslog(LOG_ERR, "ERROR: setsockopt()) %s\n", gai_strerror(status));
+        exit_program();
+        return -1;
+    }
+
+    // Bind the socket to an address
+    status = bind(socketfd, res -> ai_addr, res -> ai_addrlen);
+
+    if(status == -1)
+    {
+        syslog(LOG_ERR, "ERROR: bind() %s\n", strerror(errno));
+        return -1;
+    }
+
+    // Result is not used anymore
+    freeaddrinfo(res);
+
+    ret_val = socket_server();
+    
+    exit_program();
+
+    return ret_val;
 }
 
